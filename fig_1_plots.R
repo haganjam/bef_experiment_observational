ebd6cd2a8b7f82e227a6c4d81a799e6124c6507e
James Hagan <james_hagan@outlook.com>
2020-06-11 15:09:39 +0200

    update figures

--- fig_1_plots.R
+++ fig_1_plots.R
@@ -15,6 +15,7 @@ library(RColorBrewer)
 library(viridis)
 library(here)
 library(vegan)
+library(ggpubr)
 
 # simulate normally distributed niches
 
@@ -111,4 +112,103 @@ fig_1b <-
 
 ggsave(filename = here("figures/fig_1b.png"), plot = fig_1b,
        dpi = 300)
+
+
+# make pie charts
+
+df1 <- data.frame(
+  group = c("A", "B", "C", "D"),
+  value = c(1, 1, 1, 1)
+)
+
+p1 <- ggplot(df1, aes(x = "", y = value, fill = group)) +
+  geom_bar(width = 1, stat = "identity", colour = "black", alpha = 0.6) +
+  coord_polar("y", start=0) +
+  scale_fill_manual(values=c("#FFFFFF", "#666666", "#CCCCCC", "#000000")) +
+  theme_void() +
+  theme(legend.position = "none")
+
+df2 <- data.frame(
+  group = c("A", "D"),
+  value = c(1.5, 0.5)
+)
+
+p2 <- ggplot(df2, aes(x = "", y = value, fill = group)) +
+  geom_bar(width = 1, stat = "identity", colour = "black", alpha = 0.6) +
+  coord_polar("y", start=0) +
+  scale_fill_manual(values=c("#FFFFFF", "#000000")) +
+  theme_void() +
+  theme(legend.position = "none")
+
+df3 <- data.frame(
+  group = c("A", "B", "C"),
+  value = c(2, 1, 1)
+)
+
+p3 <- ggplot(df3, aes(x = "", y = value, fill = group)) +
+  geom_bar(width = 1, stat = "identity", colour = "black", alpha = 0.6) +
+  coord_polar("y", start=0) +
+  scale_fill_manual(values=c("#FFFFFF", "#666666", "#CCCCCC")) +
+  theme_void() +
+  theme(legend.position = "none")
+
+df4 <- data.frame(
+  group = c("B", "C"),
+  value = c(1, 1)
+)
+
+p4 <- ggplot(df4, aes(x = "", y = value, fill = group)) +
+  geom_bar(width = 1, stat = "identity", colour = "black", alpha = 0.6) +
+  coord_polar("y", start=0) +
+  scale_fill_manual(values=c("#666666", "#CCCCCC")) +
+  theme_void() +
+  theme(legend.position = "none")
+
+
+df5 <- data.frame(
+  group = c("B", "C", "D"),
+  value = c(2, 1, 1)
+)
+
+p5 <- ggplot(df5, aes(x = "", y = value, fill = group)) +
+  geom_bar(width = 1, stat = "identity", colour = "black", alpha = 0.6) +
+  coord_polar("y", start=0) +
+  scale_fill_manual(values=c("#666666", "#CCCCCC", "#000000")) +
+  theme_void() +
+  theme(legend.position = "none")
+
+ggarrange(p1, p2, p3, p4, p5)
+
+
+# make singular charts
+cols <- c("#FFFFFF", "#666666", "#CCCCCC", "#000000")
+
+df_cols <- data.frame(
+  group = c("B"),
+  value = c(1)
+)
+
+df_plots <- vector("list")
+
+for (i in seq_along(1:length(cols))) {
+  
+  df_plots[[i]] <- 
+    ggplot(df_cols, aes(x = "", y = value, fill = group)) +
+    geom_bar(width = 1, stat = "identity", colour = "black", alpha = 0.6) +
+    coord_polar("y", start=0) +
+    scale_fill_manual(values=cols[i]) +
+    theme_void() +
+    theme(legend.position = "none")
+  
+}
+
+pies <- 
+  ggarrange(p1, p2, p3, p4, p5,
+          df_plots[[1]], df_plots[[2]], df_plots[[3]], df_plots[[4]])
+
+ggsave(filename = here("figures/fig_1_pies.png"), plot = pies,
+       dpi = 300)
+
+
+
 
