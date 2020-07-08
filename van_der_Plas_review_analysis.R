281e7e5cf8a39519aa1ceb30a236a3140c00dfff
James Hagan <james_hagan@outlook.com>
2020-07-08 11:51:43 +0200

    justify spatial extent classification (figure s1)

--- van_der_Plas_review_analysis.R
+++ van_der_Plas_review_analysis.R
@@ -269,7 +269,116 @@ fig_2a
 
 
 
+### figure 2b
 
+# check the spatial extent data
+
+# subset out relationships to include
+# create useful variables for plotting
+# correct a mistake in the landscape classification
+
+fig_2b_raw <- 
+  spat_comp %>%
+  filter(include == "yes") %>%
+  mutate(direction = if_else( bef_relationship == "Positive", "+",
+                              if_else( bef_relationship == "Negative", "-", " ")),
+         bef_relationship = if_else( bef_relationship == "Positive", "positive",
+                                     if_else( bef_relationship == "Negative", "negative", "neutral")),
+         spatial_extent = if_else(spatial_extent == "landscape ", "landscape", spatial_extent))
+
+fig_2b_raw$Relationship_nr %>%
+  unique() %>%
+  length()
+
+# we obtained information on spatial extent for 231 relationships
+
+# does the classification represent spatial extent?
+spat_clas <- 
+  fig_2b_raw %>%
+  mutate(lat_diff = (max_lat - min_lat),
+         lon_diff = (max_lon -min_lon))
+
+ggplot(data = spat_clas,
+       mapping = aes(x = lon_diff, y = lat_diff, colour = spatial_extent)) +
+  geom_jitter()
+
+# check the high landscape spatial extent values
+spat_clas %>%
+  filter(spatial_extent == "landscape",
+         lat_diff > 2) %>%
+  View()
+
+# max lat is 40 and not 49
+spat_clas %>%
+  filter(spatial_extent == "regional",
+         lat_diff < -10) %>%
+  View()
+
+# lat and lon measurements are switched around
+spat_clas %>%
+  filter(spatial_extent == "continental",
+         lon_diff > 200) %>%
+  View()
+
+
+# correct these mistakes
+
+# for paper 116, replace the max_lat: 49 with max_lat: 40
+fig_2b_raw$max_lat[fig_2b_raw$`paper number` == 116] <- 40
+
+# for paper 125, replace max_lat: 7 with max_lat: 53
+# for paper 125, replace min_lon: 53 with min_lon: 7
+fig_2b_raw$max_lat[fig_2b_raw$`paper number` == 125] <- 53
+fig_2b_raw$min_lon[fig_2b_raw$`paper number` == 125] <- 7
+
+# reorder the spatial extent factor so it is ascending
+fig_2b_raw$spatial_extent <- 
+  factor(fig_2b_raw$spatial_extent, levels = c("landscape", "regional", "continental", "global"))
+
+
+### figure s1
+
+fig_s1_dat <- 
+  fig_2b_raw %>%
+  mutate(lat_diff = (max_lat - min_lat),
+         lon_diff = (max_lon -min_lon))
+
+fig_s1 <- 
+  ggplot(data = rename(fig_s1_dat, `spatial extent` = spatial_extent),
+       mapping = aes(x = log(1 + lon_diff), y = log(1 + lat_diff), 
+                     colour = `spatial extent`)) +
+  geom_point(size = 2.5, alpha = 0.8, shape = 16) +
+  scale_colour_viridis_d() +
+  ylab("latitude range (ln(1 + DD))") +
+  xlab("longitude range (ln(1 + DD))") +
+  theme_classic()
+
+ggsave(filename = here("figures/fig_s1.png"), plot = fig_s1, dpi = 300,
+       width = 14, height = 10, units = "cm")
+
+
+
+# add columns for neutral, negative and positive slopes
+fig_2b_ana <- 
+  fig_2b_raw %>%
+  group_by(spatial_extent) %>%
+  mutate(pos = if_else(bef_relationship == "positive", 1, 0),
+         neu = if_else(bef_relationship == "neutral", 1, 0),
+         neg = if_else(bef_relationship == "negative", 1, 0)) %>%
+  ungroup() %>%
+  select(`paper number`, Relationship_nr, spatial_extent, pos, neu, neg)
+
+fig_2b_ana
+
+# get the observed proportions
+fig_2b_obs <- 
+  fig_2b_ana %>%
+  group_by(spatial_extent) %>%
+  summarise_at(vars(c("pos", "neu", "neg")), ~sum(., na.rm = TRUE)) %>%
+  ungroup() %>%
+  mutate(total_n = pos + neu + neg) %>%
+  gather(pos, neu, neg, key = "relationship", value = "slope") %>%
+  mutate(slope_proportion = slope/total_n)
 
 
 
