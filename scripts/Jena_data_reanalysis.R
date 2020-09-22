ab5633b5a3a3742aa946a0933eb856dff53de41a
James Hagan <james_hagan@outlook.com>
2020-09-22 17:18:24 +0200

    clean up the script

--- scripts/Jena_data_reanalysis.R
+++ scripts/Jena_data_reanalysis.R
@@ -7,37 +7,20 @@
 library(readr)
 library(dplyr)
 library(tidyr)
-library(purrr)
 library(ggplot2)
 library(broom)
-library(RColorBrewer)
 library(viridis)
 library(here)
 library(vegan)
-library(ggpubr)
-
-# create customised plotting theme
-theme_meta <- 
-  function(base_size = 12, base_family = "") {
-  theme(panel.background =  element_rect(fill = "white"), 
-        panel.border =      element_rect(fill="NA", color="black", size=0.75, linetype="solid"),
-        axis.line.x = element_line(color="black", size = 0.2),
-        axis.line.y = element_line(color="black", size = 0.2),
-        panel.grid.major =  element_blank(),
-        panel.grid.minor =  element_blank(),
-        axis.ticks.length = unit(-0.16, "cm"),
-        axis.title.x = element_text(colour ="black", size = 12, face = "plain", margin=margin(5,0,0,0,"pt")),
-        axis.title.y = element_text(colour = "black", size = 12, face = "plain", margin=margin(0,5,0,0,"pt")),
-        axis.text.x = element_text(colour = "black", size=12, face = "plain",  margin=margin(10,0,0,0,"pt")),
-        axis.text.y = element_text(colour ="black", size=12, face = "plain", margin=margin(0,10,0,0,"pt")),
-        axis.ticks.x = element_line(colour = "black", size = 0.4),
-        axis.ticks.y = element_line(colour = "black", size = 0.4))
-}
 
 # make a folder to export figures
 if(! dir.exists(here("figures"))){
   dir.create(here("figures"))
 }
+
+# where to access functions from
+source(here("scripts/function_plotting_theme.R"))
+
 
 # load the Jena biomass data
 jena_bio <- read_delim(here("data/Jena_Biomass_02-08.csv"), delim = ",")
