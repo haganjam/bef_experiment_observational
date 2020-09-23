d5837c0cb9ed260b9bbba77e7750c9ac29f80d05
James Hagan <james_hagan@outlook.com>
2020-09-23 16:50:12 +0200

    run the model with multiple competition parameters

--- scripts/stachova_leps_model_data_preparation.R
+++ scripts/stachova_leps_model_data_preparation.R
@@ -20,16 +20,30 @@ if(! dir.exists(here("figures"))){
 source(here("scripts/function_plotting_theme.R"))
 source(here("scripts/stachova_leps_model.R"))
 
-# run the model to generate sample data
+
+# run the model to generate the output data
+
+# set the number of runs to do
+n_exp <- 4
+
+sl_mod_out <- vector("list", length = n_exp)
+for (i in (1:n_exp) ) {
+  
+  sl_mod_out[[i]] <- 
+    s_l_2010_mod(reg_pool = 100,
+                 t_steps = 500, 
+                 n0 = 3,
+                 a_mean = runif(n = 1, min = 0.6, max = 1), 
+                 a_sd = 0.2, a_min = 0.2, a_max = 1.2, a_spp = 1,
+                 k_min = 3, k_max = 150,
+                 r_min = 0.01, r_max = 0.5, 
+                 lsp = c(10, 20, 30, 40, 50, 60),
+                 reps = 16)
+  
+}
+
 sl_mod_out <- 
-  s_l_2010_mod(reg_pool = 100,
-               t_steps = 1000, 
-               n0 = 3,
-               a_mean = 0.8, a_sd = 0.2, a_min = 0.2, a_max = 1.2, a_spp = 1,
-               k_min = 3, k_max = 150,
-               r_min = 0.01, r_max = 0.5, 
-               lsp = c(10, 20, 30, 40, 50, 60),
-               reps = 16)
+  bind_rows(sl_mod_out, .id = "run")
 
 # get the final time point in the model
 sl_mod_an <- 
